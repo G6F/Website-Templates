46f85287b7a25b7c7553770e6a9d1bf28e3610d5
klook <69049679+KlookKindaCute@users.noreply.github.com>
2021-08-15 01:34:01 -0400

    Update links.js

--- Website Template 7/links.js
+++ Website Template 7/links.js
@@ -1,9 +1,9 @@
 var linkList = [
-    ["#1385b6", "#111d2f", "https://steamcommunity.com/id/xxxtentacion", "steam", 0],
-    ["#e74f8b", "#361121", "https://osu.ppy.sh/users/17585019", "osu!", 0],
-    ["#fc3c91", "#fd7a64", "https://weheartit.com/suicide", "weheartit", 0],
-    ["#1DA1F2", "#0E5078", "https://soundcloud.com/suicide", "soundcloud", 0],
-    ["#fefefe", "#02a9ff", "https://anilist.co/user/suicide", "anilist", 0],
-    ["#3F0063", "#121212", "https://gamesense.pub/forums/profile.php?id=472", "gamesense", 0],
-    ["#D72483", "#792359", "https://suicide.ooo/groups/index.html", "my groups", 1]
+    ["#1385b6", "#111d2f", "https://twitter.com/klook_", "twitter", 0],
+    ["#e74f8b", "#361121", "https://discordapp.com/users/342330427509571596", "discord", 0],
+    ["#fc3c91", "#fd7a64", "https://soundcloud.com/7xt", "soundcloud", 0],
+    ["#1DA1F2", "#0E5078", "https://www.youtube.com/channel/UCdeBcM-ys6y4w-wA26mYjZQ", "youtube", 0],
+    ["#fefefe", "#02a9ff", "https://steamcommunity.com/id/selfassured/", "steam", 0],
+    ["#3F0063", "#121212", "https://pastebin.com/mY5Ps5wC", "host reviews", 0],
+    ["#D72483", "#792359", "mailto:klookuser@pm.me?subject=hey%20klook&body=hey%20klook%2C%20wanted%20to%20let%20you%20know%20you%20are%20the%20best...", "e-mail", 1]
 ];
